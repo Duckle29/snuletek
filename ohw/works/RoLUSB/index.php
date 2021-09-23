9509bc078bf4a5c32cd8e718dfb73820578d4fca
Mikkel <2756925+Duckle29@users.noreply.github.com>
2021-09-23 02:45:43 +0200

    Added RolUSB

---
+++ ohw/works/RoLUSB/index.php
@@ -0,0 +1,50 @@
+<?php require '../../essentials/head.php';?>
+<body id="RoLUSB">
+	<div id="centerPage">
+    	<div id="header">
+	        <a href="http://snuletek.org/">
+        		<img alt="Header-Img" src="../../images/ohwHeader.png" />
+            </a>
+        </div>
+
+        <div id="mainMenu">
+        	<?php require '../../essentials/topMenu.php'; ?>
+        </div>
+
+        <div id="centerSubMenu">
+        	<div id="submenu" class="submenu">
+        	</div>
+        </div>
+
+        <div id="bodydiv">
+			<div id="content">
+        		<h2>Xbox360 RoL to USB board</h2>
+				<hr />
+
+				<p>This project grew out of <a href="http://www.reddit.com/r/pcmasterrace/comments/2vwtu5/my_own_pc_in_disguise/cols5ce">this thread on reddit</a></p>
+
+				<h3>The why?</h3>
+				<p>Why was this project actually created?<br />
+              	Playing games on PC, you realize that you sometimes want to use a controller for the game, instead of a keyboard and mouse, and if you happen to have
+                had an Xbox360 before you <a href="http://www.reddit.com/r/pcmasterrace/wiki/guide">built a PC</a> and you had wireless controllers for that, you might
+                want to use those with your PC, but you'd rather not have to buy a wireless reciever, so how about using the one in the Xbox instead?
+                This board will allow you to do that, along with <a href="http://www.instructables.com/id/Diy-Xbox-wireless-controller-adapter-for-Pc/">this instructable</a>.
+                I might make my own instructable, but for now you have to figure out how stuff goes with this board in relation to the instructable.
+			    </p>
+
+				<h3>The files</h3>
+				<p>And here's the fils for the project</p>
+                <a href="files/top.png"><img src="files/top.png" /></a>&nbsp;<a href="files/bot.png"><img src="files/bot.png" /></a>
+				<ul>
+					<li><a href="files/rev1.sch">Eagle schematic</a></li>
+				    <li><a href="files/rev1.brd">Eagle board</a></li>
+                    <li><a href="files/rev1.pdf">schematic PDF</a></li>
+				</ul>
+       		</div>
+        </div>
+        <div id="footer">
+        	<?php require '../../essentials/footer.php'; ?>
+        </div>
+    </div>
+</body>
+</html>
